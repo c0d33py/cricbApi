579a85f1e5065a7227aa95bbb8116521586d5a1f
c0d33py <rootkill007@gmail.com>
2023-02-24 20:02:22 +0500

    updated

--- main.py
+++ main.py
@@ -19,34 +19,46 @@ def cricgfg():
     section = sect[0]
     main = section.find('div', class_='cb-min-bat-rw')
     # WI 92/3 (42)
-    score = main.find('span', class_='cb-font-20 text-bold').text
-    # CRR: 2.19
-    crr = main.find('span', class_='cb-font-12 cb-text-gray').text
-    # 42/3 (42) | 2.19
-    data_array.append([score.strip(), crr.strip()])
-
-    # store stats
-    stats = section.find('div', class_='cb-col cb-col-33 cb-key-st-lst')
-    stats = stats.find_all('div', class_='cb-min-itm-rw')
-    data_array.append([x.text for x in stats])
+    score_board = main.find('span', class_='cb-font-20 text-bold').text
+    # store score
+    data_array.append({
+        'score': score_board.split(' ')[2],
+        'overs': score_board.split(' ')[3],
+    })
 
     # batting cards && bowling cards
     cards = section.find_all('div', class_='cb-min-inf')
-    batting = cards[0].find_all('div', class_='cb-min-itm-rw')
-    bowling = cards[1].find_all('div', class_='cb-min-itm-rw')
+    batting = cards[0].find_all('div', class_='cb-col cb-col-100 cb-min-itm-rw')
+    bowling = cards[1].find_all('div', class_='cb-col cb-col-100 cb-min-itm-rw')
 
-    for index, batter in enumerate(batting):
-        data = [f'batter{index}'] + [x.text for x in batter.find_all('div', {'class': lambda c: c.startswith('cb-col')})]
-        data_array.append(data)
+    for batter in batting:
+        batter_data = batter.find_all('div', class_='cb-col')
+        batter = {
+            'name': batter_data[0].text,
+            'R': batter_data[1].text,
+            'B': batter_data[2].text,
+            '4s': batter_data[3].text,
+            '6s': batter_data[4].text,
+            'SR': batter_data[5].text,
+        }
+        data_array.append(batter)
 
-    for index, bowler in enumerate(bowling):
-        data = [f'bowler{index}'] + [x.text for x in bowler.find_all('div', {'class': lambda c: c.startswith('cb-col')})]
-        data_array.append(data)
+    for bowler in bowling:
+        bowler_data = bowler.find_all('div', class_='cb-col')
+        bowler = {
+            'name': bowler_data[0].text,
+            'O': bowler_data[1].text,
+            'M': bowler_data[2].text,
+            'R': bowler_data[3].text,
+            'W': bowler_data[4].text,
+            'Econ': bowler_data[5].text,
+        }
+        data_array.append(bowler)
 
     return jsonify(data_array)
 
 
 if __name__ == "__main__":
-    app.run()  # TODO: for production, use waitress or gunicorn
-    # server = Server(app.wsgi_app)
-    # server.serve()
+    # app.run()  # TODO: for production, use waitress or gunicorn
+    server = Server(app.wsgi_app)
+    server.serve()
